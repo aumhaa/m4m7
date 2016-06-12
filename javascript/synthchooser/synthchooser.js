9e41f3d6fa630d74ab038e148c171ca1dcc3ccda
aumhaa <aumhaa@gmail.com>
2016-06-12 16:43:31 -0700

    synthchooser   :   added dynamic split assignment, synth_track_name settings.

--- javascript/synthchooser/synthchooser.js
+++ javascript/synthchooser/synthchooser.js
@@ -1,7 +1,7 @@
 autowatch = 1;
 
 aumhaa = require('_base');
-var FORCELOAD = false;
+var FORCELOAD = true;
 var DEBUG = true;
 aumhaa.init(this);
 
@@ -11,6 +11,7 @@ var chain_selectors = [];
 var storage;
 var ksliders  = [];
 var kslider;
+var synth_tab;
 var lines = [];
 var presets = [{'synth':1, 'lo':0, 'hi':24, 'atouch':0, 'program':0},
 				{'synth':2, 'lo':25, 'hi':36, 'atouch':0, 'program':0},
@@ -19,51 +20,24 @@ var presets = [{'synth':1, 'lo':0, 'hi':24, 'atouch':0, 'program':0},
 
 var chosen_synth = 4;
 var synth_track_names = ['Synth1', 'Synth2', 'Synth3', 'Synth4'];
+var synthchooser_storage;
 var synth_tracks = [];
 var chain_selectors = [];
+var dynamic_gate;
+var dynamic_assign = false;
+var dyn1 = -1;
+var dyn2 = -1;
 var alive = false;
 
 function init()
 {
 	debug('synthchooser init');
-	if((!finder)||(!finder.id))
+	for(var i=0;i<4;i++)
 	{
-		finder = new LiveAPI(callback, 'live_set');
-		debug('this set id is', finder.id);
-		if(finder.id!==0)
-		{
-			var track_ids = finder.get('tracks');
-			for(var i in track_ids)
-			{
-				if(track_ids[i] == 'id')
-				{
-					delete track_ids[i];
-				}
-			}
-			debug('tracks are:', track_ids);
-			for(var i in track_ids)
-			{
-				finder.id = parseInt(track_ids[i]);
-				var name = finder.get('name');
-				debug('new finder id is:', finder.id, 'name:', finder.get('name'));
-				for(var index in synth_track_names)
-				{
-					if(name == synth_track_names[index])
-					{
-						debug('found synth track', index);
-						synth_tracks[index] = new LiveAPI(track_callback, 'live_set');
-						synth_tracks[index].id = parseInt(track_ids[i]);
-						debug(synth_tracks[index].get('devices'));
-						chain_selectors[index] = new LiveAPI(track_callback, 'live_set');
-						chain_selectors[index].id = parseInt(track_ids[i]);
-						chain_selectors[index].goto('devices', 0, 'parameters', 1);
-						debug('chain selector name:', chain_selectors[index].get('name'));
-						break;
-					}
-				}
-			}
-		}
+		synth_track_names[i] = this.patcher.getnamed('s'+(i+1)+'_name').getvalueof()[0];
 	}
+	debug('synth_track_names:', synth_track_names);
+	detect_synth_tracks();
 	for(var i=0;i<4;i++)
 	{
 		presets[i].synth = this.patcher.getnamed('synth'+(i+1)).getvalueof();
@@ -79,6 +53,10 @@ function init()
 		ksliders[i] = this.patcher.getnamed('s'+(i+1)+'_kslider');
 		//lines[i] = this.patcher.getnamed('s'+(i+1)+'_line');
 	}
+	dynamic_gate = this.patcher.getnamed('dynamic_gate');
+	dynamic_gate.message('int', 0);
+	synth_tab = this.patcher.getnamed('synth_tab');
+	synth_tab.message('int', 4);
 	deprivatize_script_functions(this);
 	alive = true;
 	update();
@@ -98,6 +76,24 @@ function _synth(val)
 	debug('synth:', val);
 	chosen_synth = val;
 	update();
+}
+
+function _synth_chooser(num, val)
+{
+	debug('synth_chooser', num, val);
+	if(val)
+	{
+		synth_tab.message('int', num);
+		dynamic_assign = true;
+		dynamic_gate.message('int', 1);
+	}
+	else
+	{
+		dynamic_assign = false;
+		dynamic_gate.message('int', 0);
+		dyn1 = -1;
+		dyn2 = -1;
+	}
 }
 
 function _change(track, synth, lo, hi, atouch, program)
@@ -131,9 +127,78 @@ function update()
 
 function anything(){}
 
+function _dynamic_assign_input(note, vel)
+{
+	if((vel > 0)&&(dynamic_assign))
+	{
+		debug('dynamic_assign_input', note, vel);
+		if(dyn1>-1)
+		{
+			dyn2 = note;
+			this.patcher.getnamed('hi'+(chosen_synth+1)).message('int', dyn2);
+		}
+		else
+		{
+			dyn1 = note;
+			this.patcher.getnamed('lo'+(chosen_synth+1)).message('int', dyn1);
+		}
+	}
+}
 
+function synth_name(num, text, name)
+{
+	debug('synth_name:', num, text, name);
+	synth_track_names[num] = name;
+	detect_synth_tracks();
+}
 
-
-
+function detect_synth_tracks()
+{
+	if((!finder)||(!finder.id))
+	{
+		finder = new LiveAPI(callback, 'live_set');
+		debug('this set id is', finder.id);
+	}
+	if(finder.id!==0)
+	{
+		finder.goto('live_set');
+		var track_ids = finder.get('tracks');
+		for(var i in track_ids)
+		{
+			if(track_ids[i] == 'id')
+			{
+				delete track_ids[i];
+			}
+		}
+		debug('tracks are:', track_ids);
+		for(var i in track_ids)
+		{
+			finder.id = parseInt(track_ids[i]);
+			var name = finder.get('name');
+			debug('new finder id is:', finder.id, 'name:', finder.get('name'));
+			for(var index in synth_track_names)
+			{
+				if(name == synth_track_names[index])
+				{
+					debug('found synth track', index);
+					if(!synth_tracks[index])
+					{
+						synth_tracks[index] = new LiveAPI(track_callback, 'live_set');
+					}
+					synth_tracks[index].id = parseInt(track_ids[i]);
+					debug(synth_tracks[index].get('devices'));
+					if(!chain_selectors[index])
+					{
+						chain_selectors[index] = new LiveAPI(track_callback, 'live_set');
+					}
+					chain_selectors[index].id = parseInt(track_ids[i]);
+					chain_selectors[index].goto('devices', 0, 'parameters', 1);
+					debug('chain selector name:', chain_selectors[index].get('name'));
+					break;
+				}
+			}
+		}
+	}
+}
 
 forceload(this);
