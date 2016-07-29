a9c68e8f936d221f26dc21328c4f99aa0aee4521
aumhaa <aumhaa@gmail.com>
2016-07-28 20:44:28 -0700

    Livid Scripts   :   Initial commit to m4m7 for Live 9.6.2, Fixes for CNTRLR_v2.

--- javascript/steppr/steppr_modtranslator.js
+++ javascript/steppr/steppr_modtranslator.js
@@ -850,6 +850,35 @@ var LiveClassNames = [
 //		  and LCD		   //
 ///////////////////////////*/
 
+
+var STEPPR_BANKS = {'InstrumentGroupDevice':[['Macro 1', 'Macro 2', 'Macro 3', 'Macro 4', 'Macro 5', 'Macro 6', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'DrumGroupDevice':[['Macro 1', 'Macro 2', 'Macro 3', 'Macro 4', 'Macro 5', 'Macro 6', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'MidiEffectGroupDevice':[['Macro 1', 'Macro 2', 'Macro 3', 'Macro 4', 'Macro 5', 'Macro 6', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'Operator':[['Osc-A Level', 'Osc-B Level', 'Osc-C Level', 'Osc-D Level', 'Transpose', 'Filter Freq', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'UltraAnalog':[['AEG1 Attack', 'AEG1 Decay', 'AEG1 Sustain', 'AEG1 Rel', 'OSC1 Semi', 'F1 Freq', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'OriginalSimpler':[['Ve Attack', 'Ve Decay', 'Ve Sustain', 'Ve Release', 'Transpose', 'Filter Freq', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'MultiSampler':[['Ve Attack', 'Ve Decay', 'Ve Sustain', 'Ve Release', 'Transpose', 'Filter Freq', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'LoungeLizard':[['M Force', 'F Release', 'F Tone Decay', 'F Tone Vol', 'Semitone', 'P Distance', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'StringStudio':[['E Pos', 'Exc ForceMassProt', 'Exc FricStiff', 'Exc Velocity', 'Semitone', 'Filter Freq', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'Collision':[['Noise Attack', 'Noise Decay', 'Noise Sustain', 'Noise Release', 'Res 1 Tune', 'Res 1 Brightness', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'InstrumentImpulse':[['1 Start', '1 Envelope Decay', '1 Stretch Factor', 'Global Time', 'Global Transpose', '1 Filter Freq', 'Mod_Chain_Pan', 'Mod_Chain_Vol', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'NoDevice':[['None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']]}
+
+var SYNTH_STEPPR_BANKS = {'InstrumentGroupDevice':[['Macro 1', 'Macro 2', 'Macro 3', 'Macro 4', 'Macro 5', 'Macro 6', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'DrumGroupDevice':[['Macro 1', 'Macro 2', 'Macro 3', 'Macro 4', 'Macro 5', 'Macro 6', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'MidiEffectGroupDevice':[['Macro 1', 'Macro 2', 'Macro 3', 'Macro 4', 'Macro 5', 'Macro 6', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'Operator':[['Osc-A Level', 'Osc-B Level', 'Osc-C Level', 'Osc-D Level', 'Transpose', 'Filter Freq', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'UltraAnalog':[['AEG1 Attack', 'AEG1 Decay', 'AEG1 Sustain', 'AEG1 Rel', 'OSC1 Semi', 'F1 Freq', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'OriginalSimpler':[['Ve Attack', 'Ve Decay', 'Ve Sustain', 'Ve Release', 'Transpose', 'Filter Freq', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'MultiSampler':[['Ve Attack', 'Ve Decay', 'Ve Sustain', 'Ve Release', 'Transpose', 'Filter Freq', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'LoungeLizard':[['M Force', 'F Release', 'F Tone Decay', 'F Tone Vol', 'Semitone', 'P Distance', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'StringStudio':[['E Pos', 'Exc ForceMassProt', 'Exc FricStiff', 'Exc Velocity', 'Semitone', 'Filter Freq', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'Collision':[['Noise Attack', 'Noise Decay', 'Noise Sustain', 'Noise Release', 'Res 1 Tune', 'Res 1 Brightness', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'InstrumentImpulse':[['1 Start', '1 Envelope Decay', '1 Stretch Factor', 'Global Time', 'Global Transpose', '1 Filter Freq', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']],
+			'NoDevice':[['None', 'None', 'None', 'None', 'None', 'None', 'ModDevice_chord_steps', 'ModDevice_chord_thin', 'ModDevice_Repeat', 'ModDevice_Groover', 'ModDevice_Random', 'ModDevice_RotSize']]}
+
+var DEVICE_BANKS = SYNTH ? SYNTH_STEPPR_BANKS : STEPPR_BANKS;
+
 var finder;
 var pns=[];
 var mps=[];
@@ -885,6 +914,14 @@ function init_device()
 {
 	outlet(0, 'receive_device', 'set_mod_device_type', SYNTH ? 'SynthSteppr' : 'DrumSteppr');
 	outlet(0, 'receive_device', 'set_number_params', 12);
+	for(var dev_type in DEVICE_BANKS)
+	{
+		for(var bank_num in DEVICE_BANKS[dev_type])
+		{
+			outlet(0, 'send_explicit', 'receive_device_proxy', 'set_bank_dict_entry', dev_type, bank_num, DEVICE_BANKS[dev_type][bank_num]);
+		}
+		//mod.Send('receive_device_proxy', 'update_parameters');
+	}
 	found_device = 0;
 	stepseq = this.patcher.getnamed('stepseq');
 	finder = new LiveAPI(callback, 'this_device');
