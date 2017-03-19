1c9746d98a92e5d1be3dbb4f2f36393e098fad77
aumhaa <aumhaa@gmail.com>
2017-03-19 01:32:12 -0700

    Skin   :   updated to prototypal notifier setup, added some brief infotext documentation.

--- javascript/skin/skin.js
+++ javascript/skin/skin.js
@@ -10,6 +10,8 @@ var FORCELOAD = true;
 var DEBUG = true;
 aumhaa.init(this);
 
+notifiers = require('notifiers');
+
 var SHOW_STORAGE = false;
 var DISPLAY_POLY = false;
 
@@ -23,8 +25,6 @@ var shifted = false;
 var alted = false;
 var cells = [];
 var pads = [];
-var KEYS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-var NOTES = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 var KEYCOLORS = [];
 var current_edit = 1;
 var current_pset = 1;
@@ -35,23 +35,29 @@ var mod_assign_mode = false;
 var this_device_id = -1;
 var current_device;
 
-var Vars = ['assignments', 'matrix', 'push_notes', 'storage', 'poly', 'note', 'mod_A', 'mod_B', 'mod_C', 'selected', 'Mask', 'color', 'midiInputGate'];
+var Vars = ['assignments', 'matrix', 'push_notes', 'storage', 'poly', 'note', 'mod_A', 'mod_B', 'mod_C', 'selected', 'Mask', 'color', 'midiInputGate', 'info_pcontrol', 'info_patcher'];
 
 var PolyVars = ['note_id', 'modA_id', 'modB_id', 'modC_id', 'note_gate', 'modA_gate', 'modB_gate', 'modC_gate', 'color', 'mask', 'modifier_assignments'];
-function Cell(x, y)
+
+
+
+CellClass = function(x, y, identifier, name, _send, args)
 {
-	var self = this;
-	this._x = x;
-	this._y = y;
+	this.add_bound_properties(this, ['_num', 'group', '_push_note', 'update_color']);
 	this._num = (x + (y*8));
 	this.group = 0;
 	this._push_note = (Math.abs(y-7)*8)+(x+36);
+	CellClass.super_.call(this, identifier, name, _send, args);
 }
 
-Cell.prototype.update_color = function()
+inherits(CellClass, ButtonClass);
+
+CellClass.prototype.update_color = function()
 {
-	mod.Send( 'grid', 'value', this._x, this._y, KEYCOLORS[this.group]);
+	this.send(KEYCOLORS[this.group]);
 }
+
+
 
 function Pad(num, patcher)
 {
@@ -76,6 +82,8 @@ Pad.prototype.update_mod_assignments = function()
 		this._mod_assigns.push(assgn.shift());
 	}
 }
+
+
 
 function init()
 {
@@ -109,12 +117,34 @@ function alive(val)
 function initialize()
 {
 	debug('skin init.');
-	//var this_device = new LiveAPI(callback, 'this_device');
-	//var current_device = new LiveAPI(current_device_callback, 'live_set');
-	//this_device_id = this_device.id;
-	//debug('this_device_id:', this_device_id);
-	//current_device.property = 'appointed_device';
+	setup_translations();
+	setup_colors();
+	setup_patchers();
+	setup_controls();
+	//init_device();
+	deprivatize_script_functions(this);
 
+	if(SHOW_STORAGE)
+	{
+		storage.message('clientwindow');
+		storage.message('storagewindow');
+	}
+
+	set_input_gate(0);
+	Grid.set_target(_ggrid);
+	Keys.set_target(_kkey);
+	get_assignment_grid();
+	update_assignment_grid();
+	update_grid();
+	update_keys();
+}
+
+function setup_translations(){}
+
+function setup_colors(){}
+
+function setup_patchers()
+{
 	for(var i in Vars)
 	{
 		script[Vars[i]] = this.patcher.getnamed(Vars[i]);
@@ -128,48 +158,134 @@ function initialize()
 	{
 		KEYCOLORS[i] = pads[i]._color.getvalueof();
 	}
-	for(var i=0;i<8;i++)
+}
+	
+function setup_controls()
+{
+	script['GridControlRegistry'] = new ControlRegistry('GridRegistry');
+	script['KeysControlRegistry'] = new ControlRegistry('KeysRegistry');
+	script['cells'] = [];
+	script['KeyButtons'] = [];
+	script['Grid'] = new GridClass(8, 8, 'Grid');
+	script['Keys'] = new GridClass(8, 1, 'Keys');
+	for(var x=0;x<8;x++)
 	{
-		//mod.Send('base_fader', 'value', i, 1);
-		cells[i]=[];
-		for(var j=0;j<8;j++)
+		cells[x] = [];
+		for(var y=0;y<8;y++)
 		{
-			cells[i][j] = new Cell(i, j);
+			var id = x+(y*8);
+			cells[x][y] = new CellClass(x, y, id, 'Cell_'+id, make_send_func('grid', 'value', x, y));
+			GridControlRegistry.register_control(id, cells[x][y]);
+			Grid.add_control(x, y, cells[x][y]);
 		}
 	}
-	get_assignment_grid();
-	update_assignment_grid();
-	set_input_gate(0);
-	if(SHOW_STORAGE)
+	for(var id=0;id<8;id++)
 	{
-		storage.message('clientwindow');
-		storage.message('storagewindow');
+		KeyButtons[id] = new ButtonClass(id, 'Key_'+id, make_send_func('key', 'value', id));
+		KeysControlRegistry.register_control(id, KeyButtons[id]);
+		Keys.add_control(id, 0, KeyButtons[id]);
 	}
-	deprivatize_script_functions(script);
-	update_grid();
-	update_keys();
 }
 
-function callback(){}
-
-function current_device_callback(args)
+function make_send_func()
 {
-	//var args = arrayfromargs(messagename, arguments);
-	debug('current_device_callback', args);
-	/*if(args[0] == 'id')
+	var args = arrayfromargs(arguments);
+	if(args.length == 4)
 	{
-		midiInputGate.message('int', Math.floor(args[1]==this_device_id));
-	}*/
+		var func = function(value)
+		{
+			//debug('value:', args, value);
+			//debug('args:', args, args instanceof Array);
+			mod.Send(args[0], args[1], args[2], args[3], value);
+		}
+	}
+	else
+	{
+		var func = function(value)
+		{
+			debug('value:', args, value);
+			mod.Send(args[0], args[1], args[2], value);
+		}
+	}
+	return func;
 }
+
+function make_gui_send_function(patcher_object, message_header)
+{
+	if(message_header.length == 2)
+	{
+		var func = function(value)
+		{
+			//value = value==undefined ? 0 : value;
+			//var message = message_header.push(value);
+			//debug('sending gui button:', 'header is:', message_header, 'value is:', value, 'message is:', [message_header[0], message_header[1], value]);
+			//#message = message_header.concat([value]);
+			patcher_object.message.apply(patcher_object, [message_header[0], message_header[1], value]);
+		}
+	}
+	return func;
+}
+
+
 
 function anything()
 {
 	//post('anything', arrayfromargs(messagename, arguments));
 }
 
-function key(num, val)
+function _grid(x, y, val){GridControlRegistry.receive(x+(y*8), val);}
+
+function _ggrid(obj)
 {
-	debug('key', num, val, '\n');
+	var x = obj._x(Grid);
+	var y = obj._y(Grid);
+	var val = obj._value;
+	//debug('grid', x, y, val);
+	if(mod_assign_mode)
+	{
+		if(val>0)
+		{
+			//pad[current_edit-1].update_mod_assignments();
+			var pos = (x+(y*8));
+			var old_val = pads[current_edit-1]._mod_assigns[pos];
+			pads[current_edit-1]._modifier_assignments.message(pos, 0, (old_val+1)%4);
+			update_grid();
+		}
+	}
+	else if(assign_mode)
+	{
+		if((val>0))
+		{
+			assignments.message(x, y, current_edit);
+			if(cells[x][y].group!=current_edit)
+			{
+				_cell = cells[x][y];
+				_cell.group = current_edit;
+				//debug('assignment for:', x, y, 'is', current_edit);
+				push_notes.message(_cell._push_note, current_edit);
+				mod.Send( 'grid', 'value', x, y, KEYCOLORS[current_edit-1]);
+			}
+		}
+	}
+	else if(alted||follow_mode)
+	{
+		if(val>0)
+		{
+			if(cells[x][y].group!=current_edit)
+			{
+				select_voice(cells[x][y].group);
+			}
+		}
+	}
+}
+
+function _key(num, val){KeysControlRegistry.receive(num, val);}
+
+function _kkey(obj)
+{
+	var num = obj._id;
+	var val = obj._value;
+	//debug('_kkey', num, val, '\n');
 	if(val > 0)
 	{
 		switch(num)
@@ -215,6 +331,61 @@ function key(num, val)
 	}
 }
 
+function shift(val)
+{
+	//debug('shift', val);
+	shifted = val>0;
+}
+
+function alt(val)
+{
+	//debug('alt', val);
+	alted = val>0;
+}
+
+function active_handlers()
+{
+	var args = arrayfromargs(arguments);
+	debug('active_handlers:', args);
+	var controlling = false;
+	for(var i in args)
+	{
+		if((args[i]=='AumPush2ModHandler')||(args[i]=='DefaultModHandler'))
+		{
+			controlling = true;
+		}
+	}
+	set_input_gate(controlling);
+}
+
+
+
+function update_grid()
+{
+	if(mod_assign_mode)
+	{
+		//var modArray = get_mod_assignments();
+		pads[current_edit-1].update_mod_assignments();
+		for(var i=0;i<8;i++)
+		{
+			for(var j=0;j<8;j++)
+			{
+				mod.Send( 'grid', 'value', i, j, pads[current_edit-1]._mod_assigns[i+(j*8)]);
+			}
+		}
+	}
+	else
+	{
+		for(var i=0;i<8;i++)
+		{
+			for(var j=0;j<8;j++)
+			{
+				mod.Send( 'grid', 'value', i, j, KEYCOLORS[cells[i][j].group-1]);
+			}
+		}
+	}
+}
+
 function update_keys()
 {
 	mod.Send('key', 'value', 0, pads[current_edit-1]._note_gate.getvalueof());
@@ -223,7 +394,7 @@ function update_keys()
 	mod.Send('key', 'value', 3, pads[current_edit-1]._modC_gate.getvalueof());
 	mod.Send('key', 'value', 5, mod_assign_mode?3:0);
 	mod.Send('key', 'value', 6, follow_mode?2:0);
-	mod.Send('key', 'value', 7, assign_mode?6:5);
+	mod.Send('key', 'value', 7, assign_mode?5:6);
 	/*for(var i=0;i<8;i++)
 	{
 		mod.Send('key', 'value', i, parseInt((current_edit == i)*KEYS[i]));
@@ -251,21 +422,6 @@ function select_voice(num)
 	update_keys();
 }
 
-function shift(val)
-{
-	debug('shift', val);
-	shifted = val>0;
-	//update_assignment_grid();
-	//set_input_gate(shifted);
-}
-
-function alt(val)
-{
-	debug('alt', val);
-	alted = val>0;
-	//update_assignment_grid();
-	//set_input_gate(alted);
-}
 
 function toggle_mod_assign()
 {
@@ -280,6 +436,7 @@ function set_mod_assign(val)
 	update_keys();
 }
 
+
 function toggle_follow()
 {
 	follow(Math.abs(follow_mode-1));
@@ -289,6 +446,7 @@ function follow(val)
 {
 	follow_mode = val>0;
 }
+
 
 function toggle_assign()
 {
@@ -301,58 +459,6 @@ function assign(val)
 	set_input_gate(assign_mode?0:1);
 }
 
-function push_grid(x, y, val)
-{
-	debug('push_grid', x, y, val);
-	base_grid(x, y, val);
-}
-
-function base_grid(x, y, val)
-{
-	debug('base_grid', x, y, val);
-	grid(x, y, val);
-}
-
-function grid(x, y, val)
-{
-	//debug('grid', x, y, val);
-	if(mod_assign_mode)
-	{
-		if(val>0)
-		{
-			//pad[current_edit-1].update_mod_assignments();
-			var pos = (x+(y*8));
-			var old_val = pads[current_edit-1]._mod_assigns[pos];
-			pads[current_edit-1]._modifier_assignments.message(pos, 0, (old_val+1)%4);
-			update_grid();
-		}
-	}
-	else if(assign_mode)
-	{
-		if((val>0))
-		{
-			assignments.message(x, y, current_edit);
-			if(cells[x][y].group!=current_edit)
-			{
-				_cell = cells[x][y];
-				_cell.group = current_edit;
-				//debug('assignment for:', x, y, 'is', current_edit);
-				push_notes.message(_cell._push_note, current_edit);
-				mod.Send( 'grid', 'value', x, y, KEYCOLORS[current_edit-1]);
-			}
-		}
-	}
-	else if(alted||follow_mode)
-	{
-		if(val>0)
-		{
-			if(cells[x][y].group!=current_edit)
-			{
-				select_voice(cells[x][y].group);
-			}
-		}
-	}
-}
 
 function get_mod_assignments()
 {
@@ -397,32 +503,6 @@ function update_assignment_grid()
 	//post_assignments();
 }
 
-function update_grid()
-{
-	if(mod_assign_mode)
-	{
-		//var modArray = get_mod_assignments();
-		pads[current_edit-1].update_mod_assignments();
-		for(var i=0;i<8;i++)
-		{
-			for(var j=0;j<8;j++)
-			{
-				mod.Send( 'grid', 'value', i, j, pads[current_edit-1]._mod_assigns[i+(j*8)]);
-			}
-		}
-	}
-	else
-	{
-		for(var i=0;i<8;i++)
-		{
-			for(var j=0;j<8;j++)
-			{
-				mod.Send( 'grid', 'value', i, j, KEYCOLORS[cells[i][j].group-1]);
-			}
-		}
-	}
-}
-
 function post_assignments()
 {
 	debug('assigns:')
@@ -437,20 +517,6 @@ function post_assignments()
 	}
 }
 
-function active_handlers()
-{
-	var args = arrayfromargs(arguments);
-	debug('active_handlers:', args);
-	var controlling = false;
-	for(var i in args)
-	{
-		if(args[i]=='AumPush2ModHandler')
-		{
-			controlling = true;
-		}
-	}
-	set_input_gate(controlling);
-}
 
 function set_input_gate(val)
 {
@@ -502,7 +568,52 @@ function _mod_assign(num, val)
 }
 
 
+INFO = "SKIN\n";
+OVERVIEW_INFO = "64 Pads may be divided into 64 different zones.\n\n\n"+
+			"Each zone has its own output note, in addition to three alternate output notes that may be triggered when different zone assigned to modify it is held down.\n\n"+
+			"A zone has several editable parameters:\n\n"+
+			"Color: the displayed color of the zone.\n"+ 
+			"Note: the note that is output when the zone is played.\n"+ 
+			"Mod_A - Mod_C:  the alternate note that is output when another zone is set to trigger a modified on selected zone.\n"+
+			"Mask Time: adjust to prevent double-triggering of notes within the zone.\n";
+KEY_INFO = "To select a zone for editing, change the first parameter knob to the target zone.\n"+
+			"To assign a grid cell to the zone, press the KEY 8 to toggle ASSIGN mode (ASSIGN mode = RED, PLAY mode = GREEN).\n"+
+			"Toggle FOLLOW mode with KEY 7.  This will automatically select the zone for editing whenever a zone is triggered.\n"+
+			"Toggle MODIFIER assign mode with KEY 6.\n"+
+			"In MODIFIER assign mode, each cell corresponds to one of the 64 zones.  Pressing a cell cycles through four possible states for each target zone:  Off, Mod_A, Mod_B, Mod_C. \n"+
+			"If any MOD source is assigned to a target zone and that zone is struck while the selected zone is being held, the target zone will trigger the corresponding MOD note instead of its original assigned note ID.\n"+
+			"\n"+
+			"The first four KEYS toggle output of NOTE, MOD_A, MOD_B, and MOD_C.  Make sure that MOD assignments are turned on here, or MOD targets will not trigger any note when struck.\n";
+
+function info()
+{
+	debug('info...');
+	info_pcontrol.message('open');
+	info_patcher.subpatcher().getnamed('info_text').message('set', [INFO, OVERVIEW, KEY_INFO]);
+}
+
+
 /*
+function SkinModule()
+{
+	var self = this;
+	//this.add_bound_properties(this, ['update', 'assign_grid', 'assign_keys', '_grid']);
+	this._grid = undefined;
+	SkinModule.super_.call(this, 'SkinModule');
+}
+
+inherits(SkinModule, Bindable);
+
+SkinModule.prototype.update = function()
+{
+}
+
+SkinModule.prototype.assign_grid = function(grid)
+{
+	this._grid = grid;
+}
+
+
 function init_device()
 {
 	mod.Send('receive_device', 'set_mod_device_type', 'Hex');
@@ -556,6 +667,8 @@ function init_device()
 	detect_drumrack();
 }
 */
+
+
 
 forceload(this);
 
