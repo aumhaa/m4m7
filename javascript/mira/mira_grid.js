4246645c2bb95f7ea112dd19d744b020875c31ae
aumhaa <aumhaa@gmail.com>
2017-10-16 00:04:52 -0700

    mira_grid   :   basic patch, keeps crashing.

---
+++ javascript/mira/mira_grid.js
@@ -0,0 +1,48 @@
+autowatch = 1;
+
+
+
+inlets = 1;
+outlets = 1;
+
+var script = this;
+
+script._name = 'mira_grid';
+
+aumhaa = require('_base');
+var FORCELOAD = true;
+var DEBUG = true;
+aumhaa.init(this);
+
+var miraCell = [];
+
+function init()
+{
+	//initialize();
+}
+
+function initialize()
+{
+	debug('initialize');
+	for(var i=0;i<64;i++)
+	{
+		miraCell[i] = this.patcher.getnamed('cell['+i+']');
+	}
+	for(var i in miraCell)
+	{
+		miraCell[i].message('bgcolor', 0, 0, 0);
+	}
+}
+
+function anything(){}
+
+function region(num, q, val)
+{
+	debug('region:', num, q, val);
+	var sendVal = val > 0 ? [1, 1, 1] : [0, 0, 0];
+	miraCell[num].message('bgcolor', sendVal);
+}
+
+
+forceload(this);
+
